f6fabf1a4cce64e813ca15922ebdd5bd0bae0142
Jacob Duckworth <jacob.formaz@gmail.com>
2026-01-31 16:08:16 +1100

    Added server and made the app.py as minimal as possible at themoment using uv and flask

---
+++ server/app.py
@@ -0,0 +1,34 @@
+# server/app.py
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+    # Required for session cookies (Flask-Login)
+    app.secret_key = "change-me-in-env"
+
+    # Allow React dev server to send/receive cookies
+    
+
+    # Cookie settings (dev-safe; tighten for prod/HTTPS)
+    app.config.update(
+        SESSION_COOKIE_HTTPONLY=True,
+    )
+
+    # Register blueprints exactly once
+    # example from old project 
+   # app.register_blueprint(auth_bp,  url_prefix="/api/auth")
+#    app.register_blueprint(config_bp, url_prefix="/api")
+ #   app.register_blueprint(run_bp, url_prefix="/api")
+ # app.register_blueprint(stats_bp, url_prefix="/api")
+  #  app.register_blueprint(profile_bp, url_prefix="/api")
+
+    # Ensure DB connection closes after each request
+   # app.teardown_appcontext(close_db)
+
+    return app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
