c82ca1e309ac5dbdb01cf706398187770c2eb6b8
Jacob Duckworth <jacob.formaz@gmail.com>
2026-01-31 16:36:14 +1100

    Adding the api to send race lap data to backend

---
+++ src/lib/api/dataPageApi.tsx
@@ -0,0 +1,20 @@
+const API_BASE = import.meta.env.VITE_API_URL;
+
+export async function analyseRaceData(laps: FormData) {
+    const url = `${API_BASE}/api/upload/`;
+    let res: Response;
+
+    try {
+    res = await fetch(url, { method: "POST", body: laps });
+    } catch (e) {
+    console.error("Network error:", e);
+    throw new Error("Network error. Is Flask running on :5000?");
+    }
+
+    if (!res.ok) {
+    const msg = await res.text().catch(() => "");
+    throw new Error(msg || `HTTP ${res.status}`);
+    }
+
+    return await res.json();
+}
