508e37ab8c60ffa490c1e3657e5092c7d6539d5a
Jacob Duckworth <jacob.formaz@gmail.com>
2026-02-12 21:06:42 +1100

    Changed around the function now i'm using brake time and pressure and returning them to then use

--- server-py/routes/lap_events.py
+++ server-py/routes/lap_events.py
@@ -3,16 +3,19 @@
 def find_brake_zone(lap):
     brake = False
     brake_on_pct = None
+    brake_on_t = None
     brake_off_pct = None
+    brake_off_t = None
     threshold = 0.05
     for sample in lap:
         if not brake and sample["brake"] >= threshold:
             brake_on_pct = sample["pct"]
+            brake_on_t = sample["t"]
             brake = True
         elif brake == True and sample["brake"] < threshold: 
             brake_off_pct = sample["pct"]
+            brake_off_t = sample["t"]
             brake = False
             break
-    brake_dist = brake_off_pct - brake_on_pct
-    return brake_dist
+    return { "brake_on_pct": brake_on_pct, "brake_on_t": brake_on_t, "brake_off_pct": brake_off_pct, "brake_off_t": brake_off_t} 
             
