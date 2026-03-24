e54104a0b036779c85472634ddcd07d67b722e8a
Jacob Duckworth <jacob.formaz@gmail.com>
2026-03-24 15:57:56 +1100

    matching is working perfectly

--- server-py/services/process_lap.py
+++ server-py/services/process_lap.py
@@ -1,4 +1,5 @@
 from services.corner_detection import CornerDetector, Corner, Brake
+import json
 
 def detect_corners(lap, rotation=0.3, not_rotating=0.03):
     corner = CornerDetector()
@@ -11,7 +12,6 @@ def detect_corners(lap, rotation=0.3, not_rotating=0.03):
             corner.open_corner(pct, t)
         elif abs(yaw_rate) <= not_rotating:
             corner.close_corner(pct, t)
-    print("heres' your corners", corner.corners)
     return corner.corners
 
 def detect_brake_zones(lap, brake_on_threshold=0.05, brake_off_threshold=0.05):
@@ -25,7 +25,6 @@ def detect_brake_zones(lap, brake_on_threshold=0.05, brake_off_threshold=0.05):
             brake.brake_on(pct, t, b)
         elif b <= brake_off_threshold:
             brake.brake_off(pct, t, b)
-    print("here's brake zones", brake.brake_zones)
     return brake.brake_zones
 
 def match_braking_to_corners1(lap):
