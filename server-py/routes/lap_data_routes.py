2966f04a5ec87f3d452556d984889b787ec9961a
Jacob Duckworth <jacob.formaz@gmail.com>
2026-03-24 15:50:42 +1100

    Fixed bug now brake matched up with corners

--- server-py/routes/lap_data_routes.py
+++ server-py/routes/lap_data_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, jsonify
 import json
 from routes.lap_events import find_brake_zones, find_corners_by_yaw_rate, match_zones, match_braking_to_corners
 
-#from services.process_lap import detect_corners, detect_brake_zones
+from services.process_lap import match_braking_to_corners1
 
 analyse_bp = Blueprint("analyse", __name__)
 
@@ -26,7 +26,8 @@ def analyse_lap_upload():
 
     fastest_samples = fastest_lap.get("samples", []) if fastest_lap else []
     reference_samples = reference_lap.get("samples", []) if reference_lap else []
- 
+
+    matched_corners1 = match_braking_to_corners1(fastest_samples) 
     fast_lap_corner_matched = match_braking_to_corners(fastest_samples)
     reference_lap_corner_mathched = match_braking_to_corners(reference_samples)
     matched_zones = match_zones(fast_lap_corner_matched, reference_lap_corner_mathched)
--- server-py/services/process_lap.py
+++ server-py/services/process_lap.py
@@ -1,4 +1,4 @@
-from services.corner_detection import CornerDetector 
+from services.corner_detection import CornerDetector, Corner, Brake
 
 def detect_corners(lap, rotation=0.3, not_rotating=0.03):
     corner = CornerDetector()
@@ -27,6 +27,22 @@ def detect_brake_zones(lap, brake_on_threshold=0.05, brake_off_threshold=0.05):
             brake.brake_off(pct, t, b)
     print("here's brake zones", brake.brake_zones)
     return brake.brake_zones
+
+def match_braking_to_corners1(lap):
+    corners = detect_corners(lap)
+    braking = detect_brake_zones(lap)
+
+    matched_corners = []
+
+    for corner in corners:
+        for brake_zone in braking:
+            if brake_zone.brake_on_pct >= corner.rotating_pct - 0.05and brake_zone.brake_off_pct <= corner.rotation_ended_pct: 
+                corner.brake_zone = brake_zone
+                braking.remove(brake_zone)
+                break
+        matched_corners.append(corner)
+    print("here's matched corners brake and corners", matched_corners)
+    return matched_corners
 
 
 
