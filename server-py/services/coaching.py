c1f4c90006e5936d0088a3c32e115d846961fb4c
Jacob Duckworth <jacob.formaz@gmail.com>
2026-03-31 17:52:46 +1100

    Added more to the function more tips and distance logic using pct right now because don't know how to get meters just yet

--- server-py/services/coaching.py
+++ server-py/services/coaching.py
@@ -3,10 +3,14 @@ def brake_coaching(corners):
     brake_tips = []
     for matched_corners in corners:
         if matched_corners.fast.brake_zone is not None and matched_corners.ref.brake_zone is not None:
+            distance = (matched_corners.fast.brake_zone.brake_on_pct - matched_corners.ref.brake_zone.brake_on_pct) * 100
             if matched_corners.fast.brake_zone.brake_on_pct > matched_corners.ref.brake_zone.brake_on_pct:
-                tip = f"Fast lap is braking later, corner: {matched_corners.corner_num}"
+                tip = f"Fast lap is braking later, distance: {distance}, corner: {matched_corners.corner_num}"
                 brake_tips.append(tip)
-            print("brake_tips", brake_tips)
+            elif matched_corners.fast.brake_zone.brake_on_pct < matched_corners.ref.brake_zone.brake_on_pct:
+                tip = f"Ref is braking later, distance: {distance}, corner: {matched_corners.corner_num}"
+                brake_tips.append(tip)
+                print("brake_tips", brake_tips)
         else:
             continue 
             
