ce1fc6a9182fd89247d2d229a8e40d12bab2f509
Jacob Duckworth <jacob.formaz@gmail.com>
2026-04-01 19:43:58 +1100

    Changing strings to dicts

--- server-py/services/coaching.py
+++ server-py/services/coaching.py
@@ -5,10 +5,14 @@ def brake_coaching(corners):
         if matched_corners.fast.brake_zone is not None and matched_corners.ref.brake_zone is not None:
             distance = (matched_corners.fast.brake_zone.brake_on_pct - matched_corners.ref.brake_zone.brake_on_pct) * 100
             if matched_corners.fast.brake_zone.brake_on_pct > matched_corners.ref.brake_zone.brake_on_pct:
-                tip = f"Fast lap is braking later, distance: {distance}, corner: {matched_corners.corner_num}"
+                tip = {
+                    "braking" : "fast lap is braking later"
+                }
                 brake_tips.append(tip)
             elif matched_corners.fast.brake_zone.brake_on_pct < matched_corners.ref.brake_zone.brake_on_pct:
-                tip = f"Ref is braking later, distance: {distance}, corner: {matched_corners.corner_num}"
+                tip = {
+                    "braking" : "ref lap is braking later"
+                }
                 brake_tips.append(tip)
                 print("brake_tips", brake_tips)
         else:
