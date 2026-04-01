c39a537b3667a9cacaead68984ea86cfe1b4ca32
Jacob Duckworth <jacob.formaz@gmail.com>
2026-04-01 21:51:50 +1100

    Changing around the check tip to have later or earlier

--- server-py/services/coaching.py
+++ server-py/services/coaching.py
@@ -7,11 +7,11 @@ def brake_marker_coaching(corners):
         ref_brake_zone = brake_zones.ref.brake_zone
 
         if fast_brake_zone is not None and ref_brake_zone is not None:
-            check_tip = "Fast" if fast_brake_zone.brake_on_pct > ref_brake_zone.brake_on_pct else "Ref"
+            check_tip = "later" if fast_brake_zone.brake_on_pct > ref_brake_zone.brake_on_pct else "earlier"
             distance = abs(fast_brake_zone.brake_on_pct - ref_brake_zone.brake_on_pct) * 100
-            tip = {
-            "Sector": brake_zones.corner_num ,"braking" : f"{check_tip} is braking later", "Distance" : distance
-            }
+
+            tip = { "Sector": brake_zones.corner_num ,"braking" : f"Ref is braking {check_tip}", "Distance" : distance}
+
             brake_tips.append(tip)
     print("brake_tips", brake_tips)
     return brake_tips
