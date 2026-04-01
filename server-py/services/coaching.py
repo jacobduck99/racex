de7675508e5199df621d73ed550a7b21f0a74e12
Jacob Duckworth <jacob.formaz@gmail.com>
2026-04-01 21:10:33 +1100

    Cleaned up function now giving ok tips

--- server-py/services/coaching.py
+++ server-py/services/coaching.py
@@ -3,18 +3,13 @@ def brake_coaching(corners):
     brake_tips = []
     for matched_corners in corners:
         if matched_corners.fast.brake_zone is not None and matched_corners.ref.brake_zone is not None:
+            check_tip = "Fast" if matched_corners.fast.brake_zone.brake_on_pct > matched_corners.ref.brake_zone.brake_on_pct else "Ref"
             distance = (matched_corners.fast.brake_zone.brake_on_pct - matched_corners.ref.brake_zone.brake_on_pct) * 100
-            if matched_corners.fast.brake_zone.brake_on_pct > matched_corners.ref.brake_zone.brake_on_pct:
-                tip = {
-                    "Sector": matched_corners.corner_num ,"braking" : "fast lap is braking later", "Distance" : distance
-                }
-                brake_tips.append(tip)
-            elif matched_corners.fast.brake_zone.brake_on_pct < matched_corners.ref.brake_zone.brake_on_pct:
-                tip = {
-                    "Sector": matched_corners.corner_num ,"braking" : "ref lap is braking later", "Distance" : distance
-                }
-                brake_tips.append(tip)
-                print("brake_tips", brake_tips)
+            tip = {
+            "Sector": matched_corners.corner_num ,"braking" : f"{check_tip} is braking later", "Distance" : distance
+            }
+            brake_tips.append(tip)
+            print("brake_tips", brake_tips)
         else:
             continue 
             
