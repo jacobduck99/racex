9cf5537eb3225d740bcb79a0294e4efc637425e5
Jacob Duckworth <jacob.formaz@gmail.com>
2026-04-01 22:54:53 +1100

    Changed tip to tips

--- server-py/services/coaching.py
+++ server-py/services/coaching.py
@@ -11,9 +11,9 @@ def brake_marker_coaching(corners, lap_dist):
             check_tip = "later" if fast_brake_zone.brake_on_pct > ref_brake_zone.brake_on_pct else "earlier"
             distance = abs(fast_brake_zone.brake_on_pct - ref_brake_zone.brake_on_pct)
             meters = int(convert_to_meters(lap_dist, distance))
-            tip = {"Sector": brake_zones.corner_num ,"braking" : f"Ref is braking {meters} meters {check_tip}"}
+            tips = {"Sector": brake_zones.corner_num ,"braking" : f"Ref is braking {meters} meters {check_tip}"}
 
-            brake_tips.append(tip)
+            brake_tips.append(tips)
     print("brake_tips", brake_tips)
     return brake_tips
             
