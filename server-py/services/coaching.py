9d5aaca84fb42c6d6cc9c91955f541a2bd5daf66
Jacob Duckworth <jacob.formaz@gmail.com>
2026-04-02 11:09:35 +1100

    Checking for when fast brake on matches ref brake on and returning right string

--- server-py/services/coaching.py
+++ server-py/services/coaching.py
@@ -8,6 +8,10 @@ def brake_marker_coaching(corners, lap_dist):
         ref_brake_zone = brake_zones.ref.brake_zone
 
         if fast_brake_zone is not None and ref_brake_zone is not None:
+            if fast_brake_zone.brake_on_pc > 1 ref_brake_zone.brake_on_pct:
+                braking_same = {"braking": f"You are braking the same as your fastest lap sector {brake_zones.corner_num}"}
+                brake_tips.append(braking_same)
+
             check_tip = "later" if fast_brake_zone.brake_on_pct > ref_brake_zone.brake_on_pct else "earlier"
             distance = abs(fast_brake_zone.brake_on_pct - ref_brake_zone.brake_on_pct)
             meters = int(convert_to_meters(lap_dist, distance))
