0ee442da74f9e713de34571625bf93f494094417
Jacob Duckworth <jacob.formaz@gmail.com>
2026-04-02 11:11:41 +1100

    Changed wording of variables and put a continue loop in to skip to next iteration

--- server-py/services/coaching.py
+++ server-py/services/coaching.py
@@ -8,14 +8,15 @@ def brake_marker_coaching(corners, lap_dist):
         ref_brake_zone = brake_zones.ref.brake_zone
 
         if fast_brake_zone is not None and ref_brake_zone is not None:
-            if fast_brake_zone.brake_on_pc > 1 ref_brake_zone.brake_on_pct:
-                braking_same = {"braking": f"You are braking the same as your fastest lap sector {brake_zones.corner_num}"}
-                brake_tips.append(braking_same)
+            if fast_brake_zone.brake_on_pct == ref_brake_zone.brake_on_pct:
+                brake_marker_same = {"braking": f"You are braking the same as your fastest lap sector {brake_zones.corner_num}"}
+                brake_tips.append(brake_marker_same)
+                continue
 
-            check_tip = "later" if fast_brake_zone.brake_on_pct > ref_brake_zone.brake_on_pct else "earlier"
+            decide_tip = "later" if fast_brake_zone.brake_on_pct > ref_brake_zone.brake_on_pct else "earlier"
             distance = abs(fast_brake_zone.brake_on_pct - ref_brake_zone.brake_on_pct)
             meters = int(convert_to_meters(lap_dist, distance))
-            tips = {"Sector": brake_zones.corner_num ,"braking" : f"You are braking {meters} meters {check_tip} compared to your fastest laps sector {brake_zones.corner_num}"}
+            tips = {"Sector": brake_zones.corner_num ,"braking" : f"You are braking {meters} meters {decide_tip} compared to your fastest laps sector {brake_zones.corner_num}"}
 
             brake_tips.append(tips)
     print("brake_tips", brake_tips)
