3bd6636645035c2e0f2bada98a6730ed1b0041b1
Jacob Duckworth <jacob.formaz@gmail.com>
2026-04-02 11:14:28 +1100

    Fixed up the string being returned

--- server-py/services/coaching.py
+++ server-py/services/coaching.py
@@ -15,8 +15,9 @@ def brake_marker_coaching(corners, lap_dist):
 
             decide_tip = "later" if fast_brake_zone.brake_on_pct > ref_brake_zone.brake_on_pct else "earlier"
             distance = abs(fast_brake_zone.brake_on_pct - ref_brake_zone.brake_on_pct)
+
             meters = int(convert_to_meters(lap_dist, distance))
-            tips = {"Sector": brake_zones.corner_num ,"braking" : f"You are braking {meters} meters {decide_tip} compared to your fastest laps sector {brake_zones.corner_num}"}
+            tips = {"braking" : f"You are braking {meters} meters {decide_tip} compared to your fastest laps sector {brake_zones.corner_num} brake marker"}
 
             brake_tips.append(tips)
     print("brake_tips", brake_tips)
