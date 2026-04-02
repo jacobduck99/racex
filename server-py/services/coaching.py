a2158f5769cff298daf72ec7172a09090cf6416c
Jacob Duckworth <jacob.formaz@gmail.com>
2026-04-02 11:36:48 +1100

    Refactored brake coaching function

--- server-py/services/coaching.py
+++ server-py/services/coaching.py
@@ -1,7 +1,7 @@
 from services.utils import convert_to_meters
 
 def brake_marker_coaching(corners, lap_dist):
-    brake_tips = []
+    coaching = []
     for brake_zones in corners:
         
         fast_brake_zone = brake_zones.fast.brake_zone
@@ -9,8 +9,9 @@ def brake_marker_coaching(corners, lap_dist):
 
         if fast_brake_zone is not None and ref_brake_zone is not None:
             if fast_brake_zone.brake_on_pct == ref_brake_zone.brake_on_pct:
+
                 brake_marker_same = {"braking": f"You are braking the same as your fastest lap sector {brake_zones.corner_num}"}
-                brake_tips.append(brake_marker_same)
+                coaching.append(brake_marker_same)
                 continue
 
             decide_tip = "later" if fast_brake_zone.brake_on_pct > ref_brake_zone.brake_on_pct else "earlier"
@@ -18,10 +19,11 @@ def brake_marker_coaching(corners, lap_dist):
 
             meters = int(convert_to_meters(lap_dist, distance))
             check_meters = "meter" if meters == 1 else "meters"
-            tips = {"braking" : f"You are braking {meters} {check_meters} {decide_tip} compared to your fastest laps sector {brake_zones.corner_num} brake marker"}
 
-            brake_tips.append(tips)
-    print("brake_tips", brake_tips)
-    return brake_tips
+            tips = {"Sector": brake_zones.corner_num, "braking" : f"You are braking {meters} {check_meters} {decide_tip} compared to your fastest laps sector {brake_zones.corner_num} brake marker"}
+
+            coaching.append(tips)
+    print("brake_tips", coaching)
+    return coaching
             
 
