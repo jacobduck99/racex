bb32321ae07a670e2219e1bd41aac8f2fc13c469
Jacob Duckworth <jacob.formaz@gmail.com>
2026-04-03 15:44:30 +1100

    Fixed check for when meters is the same on braking

--- server-py/services/coaching.py
+++ server-py/services/coaching.py
@@ -8,17 +8,18 @@ def brake_marker_coaching(corners, lap_dist):
         ref_brake_zone = brake_zones.ref.brake_zone
 
         if fast_brake_zone is not None and ref_brake_zone is not None:
-            if fast_brake_zone.brake_on_pct == ref_brake_zone.brake_on_pct:
-
-                brake_marker_same = {"braking": f"You are braking the same as your fastest lap sector {brake_zones.corner_num}"}
-                coaching.append(brake_marker_same)
-                continue
 
             decide_tip = "later" if fast_brake_zone.brake_on_pct > ref_brake_zone.brake_on_pct else "earlier"
             distance = abs(fast_brake_zone.brake_on_pct - ref_brake_zone.brake_on_pct)
 
             meters = int(convert_to_meters(lap_dist, distance))
             check_meters = "meter" if meters == 1 else "meters"
+
+            if meters == 0:
+                brake_marker_same = {"braking": f"You are braking the same as your fastest lap sector {brake_zones.corner_num}"}
+                
+                coaching.append(brake_marker_same)
+                continue
 
             tips = {"Sector": brake_zones.corner_num, "braking" : f"You are braking {meters} {check_meters} {decide_tip} compared to your fastest laps sector {brake_zones.corner_num} brake marker"}
 
