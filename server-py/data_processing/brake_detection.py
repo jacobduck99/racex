52e17be3ef88b25de34b46fda11d72aa798ab5ff
Jacob Duckworth <jacob.formaz@gmail.com>
2026-04-04 17:27:14 +1100

    Now class is hooked up to brake and corner object

--- server-py/data_processing/brake_detection.py
+++ server-py/data_processing/brake_detection.py
@@ -3,16 +3,16 @@ from typing import Optional
 
 class BrakeDetector:
     def __init__(self, brake_on_threshold=0.05, brake_off_threshold=0.05):
-        self.braking = false
+        self.braking = False
         self.brake_zones = []
         self.brake_on_threshold = brake_on_threshold
-        self.brake_on_pct = none
+        self.brake_on_pct = None
         self.max_brake_pressure = float('-inf')
         self.brake_off_threshold = brake_off_threshold
 
     def brake_on(self, pct, t, b):
         if not self.braking and b >= self.brake_on_threshold:
-            self.braking = true
+            self.braking = True
             self.brake_on_pct = pct
             self.brake_on_t = t
             self.brake_pressure = b
@@ -24,10 +24,10 @@ class BrakeDetector:
 
     def brake_off(self, pct, t, b):
         if self.braking and b <= self.brake_off_threshold:
-            self.braking = false
+            self.braking = False
             self.brake_off_pct = pct
             self.brake_off_t = t
-            completed_braking_zones = brake(self.brake_on_pct, self.brake_on_t,self.brake_off_pct, self.brake_off_t, self.max_brake_pressure)
+            completed_braking_zones = Brake(self.brake_on_pct, self.brake_on_t,self.brake_off_pct, self.brake_off_t, self.max_brake_pressure)
             self.brake_zones.append(completed_braking_zones)
 
 @dataclass
