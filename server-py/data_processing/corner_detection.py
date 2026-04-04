022bda86ef800cbaaeb04712aae5f1259608fcab
Jacob Duckworth <jacob.formaz@gmail.com>
2026-04-04 19:17:57 +1100

    Cleaned up old code in corner detection class everything works now

--- server-py/data_processing/corner_detection.py
+++ server-py/data_processing/corner_detection.py
@@ -3,28 +3,13 @@ from typing import Optional
 from services.utils import convert_to_kph
 from data_processing.brake_detection import Brake
 
-class CornerDetector:
-    def __init__(self, brake_on_threshold=0.05, brake_off_threshold=0.05, throttle_on_threshold=0.1, throttle_off_threshold=0.2):
+class CornerDetection:
+    def __init__(self): 
         self.car_rotating = False
-        self.braking = False
         self.corners = []
-        self.brake_zones = []
-        self.brake_on_threshold = brake_on_threshold
-        self.brake_on_pct = None
-        self.max_brake_pressure = float('-inf')
-        self.brake_off_threshold = brake_off_threshold
-        self.throttle_on_threshold = throttle_on_threshold
-        self.throttle_off_threshold = throttle_off_threshold
-        self.throttle_on_t = None
-        self.throttle_on_pct = None
-        self.throttle_off_t = None
-        self.throttle_off_pct = None
-        self.last_full_throttle_pcts = []
-        self.gear = None
         self.current_min_speed = float('inf')
         self.min_speed_pct = None
         self.min_speed_kph = None
-        self.throttle = []
         self.rotating_pct = None
         self.previous_corner = None
         self.merged_corners = []
@@ -36,32 +21,12 @@ class CornerDetector:
             self.rotating_t = t
             self.yaw_rate = yaw_rate
 
-    def throttle_off(self, pct, t, throttle):
-        if throttle <= self.throttle_off_threshold:
-            self.throttle_off_t = t
-            self.throttle_off_pct = pct
-            
-    def max_brake(self, b):
-        if self.braking:
-            if b > self.max_brake_pressure:
-                self.max_brake_pressure = b
-
     def min_speed(self, spd, pct):
         if self.car_rotating and self.rotating_pct is not None:
             if spd < self.current_min_speed:
                 self.current_min_speed = spd 
                 self.min_speed_pct = pct
                 self.min_speed_kph = convert_to_kph(self.current_min_speed)
-
-    def throttle_on(self, pct, t, throttle, gear):
-        if not self.braking and self.throttle_off_pct is not None:
-            if throttle >= self.throttle_on_threshold:
-                self.throttle_on_pct = pct
-                self.throttle_on_t = t
-                self.gear = gear
-                apex = Throttle(self.throttle_off_pct, self.throttle_off_t,self.throttle_on_pct, self.throttle_on_t, self.gear) 
-                self.throttle.append(apex)
-                self.throttle_off_pct = None
 
     def close_corner(self, pct, t, min_speed, yaw_rate):
         if self.car_rotating:
@@ -103,25 +68,6 @@ class CornerDetector:
                     self.previous_corner = current_corner
         self.merged_corners.append(self.previous_corner)
         return self.merged_corners
-
-@dataclass
-class Throttle:
-    throttle_off_pct: float
-    throttle_off_t: float
-    throttle_on_pct: float
-    throttle_on_t: float
-    gear: int
-
-@dataclass
-class Corner:
-    rotating_pct: float
-    rotating_t: float 
-    rotation_ended_pct: float 
-    rotation_ended_t: float
-    brake_zone: Optional[Brake] = None
-    min_speed: Optional[float] = None
-    throttle: Optional[Throttle] = None
-    yaw_rate: Optional[float] = None
 
 @dataclass
 class Matched:
