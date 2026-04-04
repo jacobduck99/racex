58f0790e7a6f1310d049d8066657c2c40695af45
Jacob Duckworth <jacob.formaz@gmail.com>
2026-04-04 17:35:24 +1100

    Added throttle class and moved methods and class from corner detection to its own file

---
+++ server-py/data_processing/throttle_detection.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+from typing import Optional
+
+class ThrottleDetector:
+    def __init__(self, throttle_on_threshold=0.1, throttle_off_threshold=0.2):
+        self.throttle_on_threshold = throttle_on_threshold
+        self.throttle_off_threshold = throttle_off_threshold
+        self.throttle_on_t = None
+        self.throttle_on_pct = None
+        self.throttle_off_t = None
+        self.throttle_off_pct = None
+        self.last_full_throttle_pcts = []
+        self.throttle = []
+        self.gear = None
+
+    def throttle_off(self, pct, t, throttle):
+        if throttle <= self.throttle_off_threshold:
+            self.throttle_off_t = t
+            self.throttle_off_pct = pct
+
+    def throttle_on(self, pct, t, throttle, gear):
+        if not self.braking and self.throttle_off_pct is not None:
+            if throttle >= self.throttle_on_threshold:
+                self.throttle_on_pct = pct
+                self.throttle_on_t = t
+                self.gear = gear
+                apex = Throttle(self.throttle_off_pct, self.throttle_off_t,self.throttle_on_pct, self.throttle_on_t, self.gear) 
+                self.throttle.append(apex)
+                self.throttle_off_pct = None
+
+@dataclass
+class Throttle:
+    throttle_off_pct: float
+    throttle_off_t: float
+    throttle_on_pct: float
+    throttle_on_t: float
+    gear: int
