9a5965ffbcffe3f76a71ecf37fb727f8c55accc1
Jacob Duckworth <jacob.formaz@gmail.com>
2026-04-08 12:24:25 +1000

    Wired up coaching tips to the coaching main function

--- server-py/services/coaching.py
+++ server-py/services/coaching.py
@@ -1,10 +1,13 @@
 from services.utils import convert_to_meters
 from services.brake_coaching import BrakeCoaching
 from services.gear_coaching import GearCoaching
+from services.throttle_coaching import ThrottleCoaching
 
 def coaching(corners, lap_dist):
     brake = BrakeCoaching(corners, lap_dist)
     gear = GearCoaching(corners) 
+    thorttle = ThrottleCoaching(corners, lap_dist)
     brake.coaching_brake_tips()
     gear.coaching_gear_tips()
+    throttle.coaching_throttle_tips()
     
