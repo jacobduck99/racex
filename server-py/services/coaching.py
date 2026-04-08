1e49ec34291aa21a6b8f6edabab091230a6621a9
Jacob Duckworth <jacob.formaz@gmail.com>
2026-04-08 12:52:57 +1000

    Looping over list and putting them with their sectors

--- server-py/services/coaching.py
+++ server-py/services/coaching.py
@@ -4,10 +4,21 @@ from services.gear_coaching import GearCoaching
 from services.throttle_coaching import ThrottleCoaching
 
 def coaching(corners, lap_dist):
+    coaching_by_corners = []
     brake = BrakeCoaching(corners, lap_dist)
     gear = GearCoaching(corners) 
     throttle = ThrottleCoaching(corners, lap_dist)
-    brake.coaching_brake_tips()
-    gear.coaching_gear_tips()
-    throttle.coaching_throttle_tips()
+    brake_tips = brake.coaching_brake_tips()
+    gear_tips = gear.coaching_gear_tips()
+    throttle_tips = throttle.coaching_throttle_tips()
+
+    for b in brake_tips:
+        coaching_by_corners.append({"sector": b["Sector"], "braking": b["braking"]})
+    print("list", coaching_by_corners)
+
+
+
+
+
+
     
