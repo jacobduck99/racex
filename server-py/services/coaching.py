fbf91ee41438a68bba9de914fa7ff62eb2ab5e46
Jacob Duckworth <jacob.formaz@gmail.com>
2026-04-09 20:47:14 +1000

    Finally got the delta working loop through and added a key and value using the index

--- server-py/services/coaching.py
+++ server-py/services/coaching.py
@@ -11,7 +11,6 @@ def coaching(corners, lap_dist):
     brake_tips = {b["sector"]: b["braking"] for b in brake.coaching_brake_tips()}
     gear_tips = {g["sector"]: g["gear"] for g in gear.coaching_gear_tips()}
     throttle_tips = {t["sector"]: {"throttle": t["throttle"]} for t in throttle.coaching_throttle_tips()}
-    print("throttle_tips", throttle_tips)
  
     all_sectors = sorted(set(brake_tips) | set(gear_tips) | set(throttle_tips))
     
@@ -30,11 +29,10 @@ def coaching(corners, lap_dist):
             entry["throttle"] = throttle_tips[sector]
         coaching_by_corners.append(entry)
 
-    for i, v in enumerate(deltas, start=1):
-        print("i", i,"v", v)
+    for i, v in enumerate(coaching_by_corners):
+        v["delta"] = deltas[i]
+        #v[delta] = i
+        #print("list", coaching_by_corners)
+    print("coaching", coaching_by_corners)
 
-
-
-
-    print("tips", coaching_by_corners)
     return coaching_by_corners    
