aa120e606cdcd0df65ec5604ea3f5689cfb824f6
Jacob Duckworth <jacob.formaz@gmail.com>
2026-04-19 11:21:41 +1000

    Deleted dead console log out of api

--- src/lib/api/dataPageApi.ts
+++ src/lib/api/dataPageApi.ts
@@ -2,7 +2,6 @@ const API_BASE = import.meta.env.VITE_API_URL;
 
 export async function analyseRaceData(session: FormData) {
     const url = `${API_BASE}/api/parseIbt`;
-    console.log("this is url", url);
     let res: Response;
 
     try {
