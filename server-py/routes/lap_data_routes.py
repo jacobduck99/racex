1fe821361d1f0f7408715ee1f38adc1bbc1db7ab
Jacob Duckworth <jacob.formaz@gmail.com>
2026-05-09 12:58:59 +1000

    Function now plugged into route and working well deleted old print statements and left a note for myself

--- server-py/routes/lap_data_routes.py
+++ server-py/routes/lap_data_routes.py
@@ -27,17 +27,17 @@ def analyse_lap_upload():
 
     sorted_laps = sorted(laps, key=lambda lap: lap.get("lapTime", float("inf")))
     fastest_lap = sorted_laps[0] if sorted_laps else None
-    print("dict f lap", fastest_lap.keys())
     reference_lap = sorted_laps[1]
-    rest_of_laps = sorted_laps[1:] #change this later to 1: 
-    #for i, lap in enumerate(rest_of_laps, start=3):
-        #print(f"lap rank {i}, time {lap.get('lapTime')}")
+    rest_of_laps = sorted_laps[1:] 
 
     fastest_samples = fastest_lap.get("samples", []) if fastest_lap else []
     reference_samples = reference_lap.get("samples", []) if reference_lap else []
 
-    for corner_num, corners in d.items():
-        print("corner num", corner_num, "corners", corners )
+    corners_by_position = build_reference_lap(rest_of_laps)
+
+    # access keys match all laps corners by index
+    #for corner_num, corners in corners_by_position.items():
+    #    print("corner num", corner_num, "corners", corners_by_position )
 
     fast_matched_corners = analyse_lap(fastest_samples, None) 
     reference_matched_corners = analyse_lap(reference_samples, fast_matched_corners)
